dde2d61f247346bfd49862885cd2f3bc47a5e053
PilipenkoEugene <70794926+PilipenkoEugene@users.noreply.github.com>
2020-12-26 11:11:47 +0300

    Add files via upload

---
+++ Lab11/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    public enum ProductType
+    {
+        Type1,
+        Type2,
+        Type3,
+        Type4
+    }
+    public class Person
+    {
+        public string ID { get; set; }
+        string Fullname { get; set; }
+        public ProductType? Product { get; set; }
+        public float Salary { get; set; }
+        public int ProductionAmount { get; set; }
+        public int ProductPrice { get; set; }
+
+        public Person(string str)
+        {
+            var e = str.Split(',');
+            ID = e[0].Trim();
+            Fullname = e[1].Trim();
+            ProductType tmp;
+            var result = Enum.TryParse(e[2].Trim(), out tmp);
+            Product = tmp;
+            if (!result)
+                Product = null;
+            Salary = Convert.ToSingle(e[3].TrimStart('$').Replace('.', ','));
+            ProductionAmount = Convert.ToInt32(e[4].Trim());
+            ProductPrice = Convert.ToInt32(e[5].Trim());
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format(
+                "*******************************************************\n" +
+                "ID: {0}, Сотрудник: {1}, Категория продукта: {2}, Зарплата: {3}, Количество произведенных: {4}, Цена одного: {5})",
+                ID, Fullname, Product.ToString(), Salary, ProductionAmount, ProductPrice);
+            return s;
+        }
+
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fIn = new StreamReader("data_example_small.csv");
+#if !DEBUG
+            TextWriter saveOut = Console.Out;
+            var newOut = new StreamWriter(@"lr11Output.txt");
+            Console.SetOut(newOut);
+#endif
+            var all = new List<Person>();
+            
+            try
+            {
+                string line = fIn.ReadLine();
+                while ((line = fIn.ReadLine()) != null)
+                {
+                    all.Add(new Person(line));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Всего сотрудников: {0}", all.Count);
+            foreach (var p in all)
+                Console.WriteLine(p);
+
+            Console.WriteLine("********Задача 1 *********");
+            var count = all.Where(x => x.Salary < x.ProductionAmount * x.ProductPrice).Count();
+            Console.WriteLine("количество тех кто получает меньше чем вырабатывает: {0}", count);
+
+            Console.WriteLine("********Задача 2 *********");
+            var withoutType = all.Where(x => x.Product == null).Sum(x => x.ProductionAmount);
+            Console.WriteLine("Количество выпущенных товаров без категории {0}", withoutType);
+
+            Console.WriteLine("********Задача 3 *********");
+            Console.WriteLine("Общая стоимость всех продуктов категории " + ProductType.Type1 + ": " +
+                all.Where(x => x.Product == ProductType.Type1).Sum(x => x.ProductionAmount * x.ProductPrice));
+            Console.WriteLine("Общая стоимость всех продуктов категории " + ProductType.Type2 + ": " +
+                all.Where(x => x.Product == ProductType.Type2).Sum(x => x.ProductionAmount * x.ProductPrice));
+            Console.WriteLine("Общая стоимость всех продуктов категории " + ProductType.Type3 + ": " +
+                all.Where(x => x.Product == ProductType.Type3).Sum(x => x.ProductionAmount * x.ProductPrice));
+            Console.WriteLine("Общая стоимость всех продуктов категории " + ProductType.Type4 + ": " +
+                all.Where(x => x.Product == ProductType.Type4).Sum(x => x.ProductionAmount * x.ProductPrice));
+
+            Console.WriteLine("********Задача 4 *********");
+            Func<Person, float> getDiff = min => min.ProductionAmount * min.ProductPrice - min.Salary;
+            var LeastEffective = all.Aggregate((mi, x) => getDiff(mi) < getDiff(x) ? mi : x);
+            Console.WriteLine("Самый неэффективный сотрудник " + LeastEffective.ID);
+
+
+
+#if !DEBUG
+            Console.SetOut(saveOut);
+            newOut.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
